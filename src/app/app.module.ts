366fdca47b7128fa4711dba54c16966a9acd299f
System Administrator <root@academy-intern-mac46s-MacBook-Air.local>
2021-06-04 15:51:02 +0200

    new components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
